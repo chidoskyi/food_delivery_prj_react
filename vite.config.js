cdec2ee710d11570352ddfc712b729c83480d2c6
okpala chidiebere <okpalachidiebere99@gmail.com>
2025-01-04 01:41:54 +0100

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite'
-import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react(),],
   build: {
     outDir: 'dist', // Ensure this is the correct directory
   },
-  plugins: [react(),  visualizer({
-    open: true, // Automatically opens the visualization in your browser
-    filename: 'stats.html', // Output file name
-    gzipSize: true, // Display gzip size of each module
-    brotliSize: true, // Display brotli size of each module
-  }),],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
